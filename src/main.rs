afa4cf048b1910b06485c14d8c22d63f74582071
Leonard Grosoli <hileo77@gmail.com>
2022-01-25 22:15:46 +0100

    test

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use bevy::{
+    pbr::wireframe::{WireframeConfig, WireframePlugin},
+    prelude::*,
+    render::{mesh::Indices, render_resource::PrimitiveTopology},
+};
+
+fn main() {
+    App::new()
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(WireframePlugin)
+        .add_startup_system(setup)
+        .run();
+}
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut wireframe_config: ResMut<WireframeConfig>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    wireframe_config.global = true;
+
+    let extent = 1243.0;
+    let vertices = [
+        ([extent, 0.0, -extent], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([extent, 0.0, extent], [0.0, 1.0, 0.0], [1.0, 0.0]),
+        ([-extent, 0.0, extent], [0.0, 1.0, 0.0], [0.0, 0.0]),
+        ([-extent, 0.0, -extent], [0.0, 1.0, 0.0], [0.0, 1.0]),
+    ];
+
+    let indices = Indices::U32(vec![0, 2, 1, 0, 3, 2]);
+
+    let mut positions = Vec::new();
+    let mut normals = Vec::new();
+    let mut uvs = Vec::new();
+    for (position, normal, uv) in vertices.iter() {
+        positions.push(*position);
+        normals.push(*normal);
+        uvs.push(*uv);
+    }
+
+    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+    mesh.set_indices(Some(indices));
+    mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+    mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+    mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+
+    // add entities to the world
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(mesh),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..Default::default()
+    });
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
+        ..Default::default()
+    });
+    // camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_translation(Vec3::new(0.0, 0.5, 5.0))
+            .looking_at(Vec3::default(), Vec3::Y),
+        ..Default::default()
+    });
+}
